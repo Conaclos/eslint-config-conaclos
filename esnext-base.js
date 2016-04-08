3720716339276697dfe79d5713cae42553f226bf
Victorien Elvinger <victorien@elvinger.fr>
2016-04-08 20:00:55 +0200

    refactor: prefer use the default parser instead of babel-eslint. Proper dependency management

--- esnext-base.js
+++ esnext-base.js
@@ -4,9 +4,9 @@ var path = require("path")
 
 module.exports = {
     "extends": path.join(__dirname, "base.js"),
-    parser: "babel-eslint",
-    plugins: ["babel"],
-    sourceType: "module",
+    parserOptions: {
+        sourceType: "module"
+    },
     rules: {
         "no-var": 2,
         "prefer-const": 2,
--- esnext-style.js
+++ esnext-style.js
@@ -1,7 +1,6 @@
 "use strict"
 
 var path = require("path")
-var styleRules = require("./style").rules
 
 module.exports = {
     "extends": [
@@ -11,24 +10,8 @@ module.exports = {
     rules: {
         "comma-dangle": [2, "always-multiline"],
             // Better diff
-            // Babel remove them
-        "prefer-arrow-callback": 2,
-
-    // Disable builtin rules that are incompatible with Babel
-        "generator-star-spacing": 0,
-        "new-cap": 0,
-        "array-bracket-spacing": 0,
-        "object-curly-spacing": 0,
-        "object-shorthand": 0,
-        "arrow-parens": 0,
-
-        "babel/generator-star-spacing": styleRules["generator-star-spacing"],
-        "babel/new-cap": styleRules["new-cap"],
-        "babel/array-bracket-spacing": styleRules["array-bracket-spacing"],
-        "babel/object-curly-spacing": styleRules["object-curly-spacing"],
-        "babel/object-shorthand": [2, "methods"],
-        "babel/arrow-parens": styleRules["arrow-parens"],
-        "babel/no-await-in-loop": 2
+            // Transpilers remove them
+        "prefer-arrow-callback": 2
     }
 }
 
