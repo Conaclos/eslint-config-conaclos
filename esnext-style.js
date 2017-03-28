feeb96e136a2800376d504148d132a84a24958a7
Victorien Elvinger <victorien@elvinger.fr>
2017-03-28 23:10:52 +0200

    refactor(esnext-style): relax comma-dangle
    accept comma dangle in multiline but don't enforce it.

--- esnext-style.js
+++ esnext-style.js
@@ -8,7 +8,7 @@ module.exports = {
         path.join(__dirname, "esnext-base.js")
     ],
     rules: {
-        "comma-dangle": [2, "always-multiline"],
+        "comma-dangle": [2, "only-multiline"],
             // Better diff
             // Transpilers remove them
         "prefer-arrow-callback": 2
