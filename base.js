ecbd2d4ad2df264b0aab9fc6d3c20115569453c1
Victorien Elvinger <victorien@elvinger.fr>
2017-07-02 18:44:22 +0200

    refactor(base): align with eslint-recommended
    BREAKING CHANGE: disable useless char escape

--- base.js
+++ base.js
@@ -165,6 +165,7 @@ module.exports = {
         "no-unused-labels": 2,
         "no-useless-call": 2,
         "no-useless-concat": 2,
+        "no-useless-escape": 2,
         "no-useless-return": 2,
         "no-implicit-globals": 2,
         "no-param-reassign": 2,
