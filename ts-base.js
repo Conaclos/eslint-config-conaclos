18d7b2d604808528de93934f9895bfd99fa4a7cc
Victorien Elvinger <victorien@elvinger.fr>
2020-02-17 16:26:01 +0100

    style(ts-base): change rule order

--- ts-base.js
+++ ts-base.js
@@ -9,30 +9,29 @@ module.exports = {
         "@typescript-eslint"
     ],
     rules: {
-        "no-dupe-class-members": 0,
-            // signatures overloadings are reported as duplicated class members.
-
-        // Obsolete TypeScript
+        "@typescript-eslint/adjacent-overload-signatures": 2,
+        "@typescript-eslint/ban-types": 2,
         "@typescript-eslint/consistent-type-assertions": 2,
+        "@typescript-eslint/no-empty-interface": 2,
+        "@typescript-eslint/no-explicit-any": 2,
+        "@typescript-eslint/no-extraneous-class": 2,
+        "@typescript-eslint/no-misused-new": 2,
+        "@typescript-eslint/no-non-null-assertion": 2,
+        "@typescript-eslint/no-var-requires": 2,
+        "@typescript-eslint/prefer-namespace-keyword": 2,
 
-        // Possible error
+        // Extension rules
         "no-array-constructor": 0,
         "@typescript-eslint/no-array-constructor": 2,
-        "@typescript-eslint/no-extraneous-class": 2,
-        "@typescript-eslint/no-empty-interface": 2,
+
+        "no-dupe-class-members": 0,
+            // signatures overloadings are reported as duplicated class members.
+        "@typescript-eslint/no-dupe-class-members": 2,
+
         "no-unused-vars": 0,
         "@typescript-eslint/no-unused-vars": [2, {
             vars: "all",
             args: "none"
-        }],
-
-        // Best practice
-        "@typescript-eslint/adjacent-overload-signatures": 2,
-        "@typescript-eslint/ban-types": 2,
-        "@typescript-eslint/no-explicit-any": 2,
-        "@typescript-eslint/no-misused-new": 2,
-        "@typescript-eslint/no-non-null-assertion": 2,
-        "@typescript-eslint/no-var-requires": 2,
-        "@typescript-eslint/prefer-namespace-keyword": 2
+        }]
     }
 }
