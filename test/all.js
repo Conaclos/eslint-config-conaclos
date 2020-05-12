20f21591a300e60936a6ad4ac1db401d5db6e67e
Victorien Elvinger <victorien@elvinger.fr>
2020-05-12 20:00:49 +0200

    feat(eslint): upgrade eslint and related packages

--- test/all.js
+++ test/all.js
@@ -1,7 +1,7 @@
 
 import test from "ava"
 import eslint from "eslint"
-import tempWrite from "temp-write"
+//import tempWrite from "temp-write"
 
 import baseConf from "../base"
 import esnextBaseConf from "../esnext-base"
@@ -10,46 +10,46 @@ import distModConf from "../dist-mod"
 import browserModConf from "../browser-mod"
 import nodeModConf from "../node-mod"
 
-test("base", (t) => {
+test("base", async (t) => {
     const conf = baseConf
 
     t.true(isPlain(conf))
-    t.is(eslintResults(conf, '"use strict";').length, 0)
+    t.is((await eslintResults(conf, '"use strict";')).length, 0)
 })
 
-test("esnext-base", (t) => {
+test("esnext-base", async (t) => {
     const conf = esnextBaseConf
 
     t.true(isPlain(conf))
-    t.is(eslintResults(conf, "").length, 0)
+    t.is((await eslintResults(conf, "")).length, 0)
 })
 
-test("ts-base", (t) => {
+test("ts-base", async (t) => {
     const conf = tsBaseConf
 
     t.true(isPlain(conf))
-    t.is(eslintResults(conf, "", "@typescript-eslint/parser").length, 0)
+    t.is((await eslintResults(conf, "")).length, 0)
 })
 
-test("dist-mod", (t) => {
+test("dist-mod", async (t) => {
     const conf = distModConf
 
     t.true(isPlain(conf))
-    t.is(eslintResults(conf, "").length, 0)
+    t.is((await eslintResults(conf, "")).length, 0)
 })
 
-test("browser-only-mod", (t) => {
+test("browser-only-mod", async (t) => {
     const conf = browserModConf
 
     t.true(isPlain(conf))
-    t.is(eslintResults(conf, "").length, 0)
+    t.is((await eslintResults(conf, "")).length, 0)
 })
 
-test("node-only-mod", (t) => {
+test("node-only-mod", async (t) => {
     const conf = nodeModConf
 
     t.true(isPlain(conf))
-    t.is(eslintResults(conf, "").length, 0)
+    t.is((await eslintResults(conf, "")).length, 0)
 })
 
 // Utils
@@ -69,14 +69,13 @@ function isPlain (o) {
  * @return {string[]} List of reported errors and warning of the execution of
  *      ESLint on `aCode' with `aConf' as configuration
  */
-function eslintResults (aConf, aCode, parser = "espree") {
-    const cli = new eslint.CLIEngine({
-        parser: parser,
+async function eslintResults (aConf, aCode) {
+    const cli = new eslint.ESLint({
         useEslintrc: false,
-        configFile: tempWrite.sync(JSON.stringify(aConf)),
+        baseConfig: aConf,
     })
-    const outputs = cli.executeOnText(aCode).results[0]
+    const results = await cli.lintText(aCode)
 
-    return outputs.messages.map((msg) => msg.ruleId)
+    return results[0].messages.map((msg) => msg.ruleId)
 }
 
